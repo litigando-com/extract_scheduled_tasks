f01e0b3fba87af1092881276ead7a268c602c78d
ncastellanosort <nicolasfelipe00234@gmail.com>
2026-02-18 17:27:10 -0500

    chore: started parsing tasks

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
+# schtasks /create /sc once /tn "prueba_sencilla" /tr "notepad.exe" /st 18:00
 import subprocess
 
-def main():
+def read_file():
     with open("tasks.txt", "w") as file:
         try:
             result = subprocess.run(
                 ["schtasks", "/query"],
                     capture_output=True,
-                    text=True,
-                    errors=None
+                    text=True
                 )
 
             lines = result.stdout.split("\n")
             
-            for line in lines:
-                file.write(f"{line}\n")
-                # print(f"{line}\n")
+            print("Reading file...\n")
 
+            for line in lines:
+                # file.write(f"{line}\n")
+                print(f"{line}")
+
+            print("Processes inserted into tasks.txt!\n")
 
         except Exception as e:
-            print(f"error {e}")
+            print(f"error with the file: {e}")
+
+def main():
+    print("=============================")
+    print("\n== Read tasks from Windows ==\n")
+    print("=============================")
+
+    read_file();
 
 if __name__ == "__main__":
     main()
